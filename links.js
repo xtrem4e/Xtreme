668a8c114ea7e2a27092677e13d39e9ccb6267c8
xtrem4e <xtrem4e@gmail.com>
2026-01-16 00:08:58 +0100

    Update links.js

--- links.js
+++ links.js
@@ -6,6 +6,7 @@ const adsterraLink = "https://is.gd/axqRCd";
 const myFiles = [
     { name: "Jujutsu Kaisen: S3E01", size: "180.20 MB", url: "https://kwik.cx/f/h8KC7LRY2eFw" },
     { name: "Jujutsu Kaisen: S3E02", size: "147.62 MB", url: "https://kwik.cx/f/FRHHYpeUx3Kz" },
+    { name: "Jujutsu Kaisen: S3E03", size: "160.67 MB", url: "https://kwik.cx/f/P0aofLOSYjhW" },
     
     
     
