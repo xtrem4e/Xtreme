c593e490d843a372fba226050c7caa6637118bfd
xtrem4e <xtrem4e@gmail.com>
2026-01-23 02:57:57 +0100

    Update links.js

--- links.js
+++ links.js
@@ -7,7 +7,7 @@ const myFiles = [
     { name: "Jujutsu Kaisen: S3E01", size: "180.20 MB", url: "https://kwik.cx/f/h8KC7LRY2eFw" },
     { name: "Jujutsu Kaisen: S3E02", size: "147.62 MB", url: "https://kwik.cx/f/FRHHYpeUx3Kz" },
     { name: "Jujutsu Kaisen: S3E03", size: "160.67 MB", url: "https://kwik.cx/f/P0aofLOSYjhW" },
-    
+    { name: "Jujutsu Kaisen: S3E04", size: "225.36 MB", url: "https://kwik.cx/f/pz28Hh2u0xRa" },
     
     
     // To add more, just paste a new line below this one:
